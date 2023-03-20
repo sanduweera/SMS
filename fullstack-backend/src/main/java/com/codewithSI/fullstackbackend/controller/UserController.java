77a83e9974f40c7726cec83942abd5642ff3102f
sanduweera <sanduniweera2@gmail.com>
2023-03-20 22:03:28 +0530

    g
    gg

--- fullstack-backend/src/main/java/com/codewithSI/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/codewithSI/fullstackbackend/controller/UserController.java
@@ -3,25 +3,33 @@ package com.codewithSI.fullstackbackend.controller;
 import com.codewithSI.fullstackbackend.exception.UserNotFoundException;
 import com.codewithSI.fullstackbackend.model.User;
 import com.codewithSI.fullstackbackend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
+
+    Logger logger = (Logger) (Logger) LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
+        logger.trace("New User Is Added");
         return userRepository.save(newUser);
     }
 
     @GetMapping("/users")
     List<User> getAllUsers(){
+        logger.trace("List of All Users");
         return userRepository.findAll();
     }
 
@@ -33,6 +41,7 @@ public class UserController {
 
     @PutMapping("/user/{id}")
     User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        logger.trace("User Details are Updated");
         return userRepository.findById(id)
                 .map(user -> {
                     user.setLastname(newUser.getLastname());
@@ -47,6 +56,7 @@ public class UserController {
 
     @DeleteMapping("/user/{id}")
     String deleteUser(@PathVariable Long id){
+        logger.trace("User is Deleted");
         if(!userRepository.existsById(id)){
             throw new UserNotFoundException(id);
         }
